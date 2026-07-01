b7a3d7075e2eae5166504686de6dc48b1c413b4d
Kanav Dumra <kanavdumra@gmail.com>
2026-07-01 13:31:54 +0530

    Initial commit

---
+++ Code.ino
@@ -0,0 +1,177 @@
+/*obstacle avoiding, Bluetooth control, voice control robot car.
+   Home Page
+*/
+#include <Servo.h>
+#include <AFMotor.h>
+#define Echo A0
+#define Trig A1
+#define motor 10
+#define Speed 100
+#define spoint 103
+char value;
+int distance;
+int Left;
+int Right;
+int L = 0;
+int R = 0;
+int L1 = 0;
+int R1 = 0;
+Servo servo;
+AF_DCMotor M1(1);
+AF_DCMotor M2(2);
+AF_DCMotor M3(3);
+
+AF_DCMotor M4(4);
+void setup() {
+  Serial.begin(9600);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+  servo.attach(motor);
+  M1.setSpeed(Speed);
+  M2.setSpeed(Speed);
+  
+}
+void loop() {
+  
+  //Obstacle();
+  Bluetoothcontrol();
+  voicecontrol();
+}
+void Bluetoothcontrol() {
+  if (Serial.available() > 0) {
+    value = Serial.read();
+    Serial.println(value);
+  }
+  if (value == 'F') {
+    forward();
+  } else if (value == 'B') {
+    backward();
+  } else if (value == 'L') {
+    left();
+  } else if (value == 'R') {
+    right();
+  } else if (value == 'S') {
+    Stop();
+  }
+}
+void Obstacle() {
+  distance = ultrasonic();
+  if (distance <= 12) {
+    Stop();
+    backward();
+    delay(100);
+    Stop();
+    L = leftsee();
+    servo.write(spoint);
+    delay(800);
+    R = rightsee();
+    servo.write(spoint);
+    if (L < R) {
+      right();
+      delay(500);
+      Stop();
+      delay(200);
+    } else if (L > R) {
+      left();
+      delay(500);
+      Stop();
+      delay(200);
+    }
+  } else {
+    forward();
+  }
+}
+void voicecontrol() {
+  distance = ultrasonic();
+  if (distance <= 12) {
+    Stop();
+    
+    
+  } else {
+    if (Serial.available() > 0) {
+    value = Serial.read();
+    Serial.println(value);
+    if (value == '^') {
+      forward();
+    } 
+    else if (value == '-') {
+      backward();
+    } 
+    else if (value == '<') {
+      L = leftsee();
+      servo.write(spoint);
+      if (L >= 10 ) {
+        left();
+        delay(500);
+        Stop();
+      } else if (L < 10) {
+        Stop();
+      }
+    } 
+    else if (value == '>') {
+      R = rightsee();
+      servo.write(spoint);
+      if (R >= 10 ) {
+        right();
+        delay(500);
+        Stop();
+      } else if (R < 10) {
+        Stop();
+      }
+    } else if (value == '*') {
+      Stop();
+    }
+    
+  }
+  
+  
+  }
+}
+// Ultrasonic sensor distance reading function
+int ultrasonic() {
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(4);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+  long t = pulseIn(Echo, HIGH);
+  long cm = 0.017 * t; //time convert distance
+  return cm;
+}
+void forward() {
+  M1.run(FORWARD);
+  M2.run(FORWARD);
+  
+}
+void backward() {
+  M1.run(BACKWARD);
+  M2.run(BACKWARD);
+  
+}
+void right() {
+  M1.run(FORWARD);
+  M2.run(BACKWARD);
+  
+}
+void left() {
+  M1.run(BACKWARD);
+  M2.run(FORWARD);
+  
+}
+void Stop() {
+  M1.run(RELEASE);
+  M2.run(RELEASE);
+  
+}
+int rightsee() {
+  servo.write(20);
+  delay(800);
+  Left = ultrasonic();
+  return Left;
+}
+int leftsee() {
+  servo.write(180);
+  delay(800);
+  Right = ultrasonic();
+  return Right;
+}
